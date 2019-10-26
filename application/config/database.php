3d47de15c2e0a27db4f18f3ab411ab15144ef848
“Wendell <wendell.rodrigues@sjsu.edu>
2019-10-26 19:46:38 +0000

    Changed

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '192.168.33.30',
+	'hostname' => 'localhost',
 	'username' => 'ci_user',
 	'password' => 'cipassword',
 	'database' => 'ci_database',
