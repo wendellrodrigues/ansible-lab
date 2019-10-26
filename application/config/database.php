4b6737834cc93aef1f543d735a652a76bee3564f
“Wendell <wendell.rodrigues@sjsu.edu>
2019-10-26 20:44:12 +0000

    Fixed the database.php error

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '192.168.33.30',
 	'username' => 'ci_user',
 	'password' => 'cipassword',
 	'database' => 'ci_database',
